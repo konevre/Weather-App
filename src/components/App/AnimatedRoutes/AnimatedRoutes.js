d590bade7d54bc0a825fa5bc4c57eb33619b73b4
konevre <konevre@mail.ru>
2023-01-02 00:18:17 +0700

    added 404 page

--- src/components/App/AnimatedRoutes/AnimatedRoutes.js
+++ src/components/App/AnimatedRoutes/AnimatedRoutes.js
@@ -6,18 +6,20 @@ import Map from '../../Map/Map';
 import Weather from '../../Weather/Weather';
 import Cities from '../../Cities/Cities';
 import Settings from "../../Settings/Settings";
+import Page404 from "../../Page404/Page404";
 
 const AnimatedRoutes = () => {
     const location = useLocation();
 
     return (
-        <AnimatePresence exitBeforeEnter>
+        <AnimatePresence mode='wait'>
             <Routes location={location} key={location.pathname}>
 				<Route path="/" element={<Preview/>}/>
 				<Route path="/weather" element={<Weather/>}/>
 				<Route path="/cities" element={<Cities/>}/>
 				<Route path="/map" element={<Map/>}/>
 				<Route path="/settings" element={<Settings/>}/>
+				<Route path="*" element={<Page404/>}/>
 			</Routes>
         </AnimatePresence>
     )
